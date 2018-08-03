cffa6a2e1f731e1af27d4a6cda8fce92b93747fc
Robert1211 <maculewicz.au@gmail.com>
2018-08-03 14:47:28 +1000

    login/half

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,3 +1,41 @@
-class UsersController < ApplicationController
+class SuppliersController < ApplicationController
+ # before_action :check_for_admin, :only => [:index]
 
+ def index
+  @suppliers = Supplier.all
+ end
+
+ def new
+  @suppliers = Supplier.new
+ end
+
+ # def create
+ #  @suppliers = Supplier.new user_params
+ #  if @supplier.save
+ #    session[:supplier_id] = @supplier.id
+ #    redirect_to new_home_path                    # ask boys about path
+ #  else
+ #    render :new
+ #  end
+ # end
+
+  def create
+   @supplier = Supplier.new(supplier_params)
+
+    respond_to do |format|
+     if @supplier.save
+      SupplierMailer.welcome(@supplier).deliver_now
+      format.html { redirect_to @supplier, notice: 'Contractor was created.'}
+      format.json { render :show, status: :created, location: @supplier }
+    else
+      format.html { render :new }
+      format.json { render json: @user.errors, status:
+      :unprocessable_entity }
+     end
+   end
+ end
+   private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root :to => 'suppliers#index'
 
+
+    get '/login' => 'session#new'
+    post '/login' => 'session#create'
+    delete '/login' => 'session#destroy'
+
+    get '/session' => 'suppliers#index'
 
 end
