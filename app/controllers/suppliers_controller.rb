2a03626b21395fea6af20d42eba5d7d68347ab54
Robert1211 <maculewicz.au@gmail.com>
2018-08-06 09:09:00 +1000

    login_logout

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,12 +1,12 @@
-class SuppliersController < ApplicationController
- # before_action :check_for_admin, :only => [:index]
+class SuppliersController <    ApplicationController
+  # before_action :check_for_admin, :only => [:index]
 
  def index
   @suppliers = Supplier.all
  end
 
  def new
-  @suppliers = Supplier.new
+  @supplier = Supplier.new
  end
 
  # def create
@@ -24,18 +24,18 @@ class SuppliersController < ApplicationController
 
     respond_to do |format|
      if @supplier.save
-      SupplierMailer.welcome(@supplier).deliver_now
-      format.html { redirect_to @supplier, notice: 'Contractor was created.'}
+      # SupplierMailer.welcome(@supplier).deliver_now
+      format.html { redirect_to root_path, notice: 'Contractor was created.'}
       format.json { render :show, status: :created, location: @supplier }
     else
       format.html { render :new }
-      format.json { render json: @user.errors, status:
+      format.json { render json: @supplier.errors, status:
       :unprocessable_entity }
      end
    end
  end
    private
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    def supplier_params
+      params.require(:supplier).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'suppliers#index'
 
-
+  resources :suppliers, :only => [:new, :create, :index] # 7 CRUD methods for suppliers
     get '/login' => 'session#new'
     post '/login' => 'session#create'
     delete '/login' => 'session#destroy'
