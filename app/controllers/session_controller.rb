c7bba1d2ba802fbec9f7c3dd7d3047ce7edbecd0
Robert1211 <maculewicz.au@gmail.com>
2018-08-07 09:31:41 +1000

    last_changee

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,18 +1,30 @@
 class SessionController < ApplicationController
+   skip_before_action :verify_authenticity_token, :only => [:create]
    def new
     end
 
+    def index
 
+      @supplier = Supplier.find(params[:id])
+      render :action => 'index.json'
+    end
+
+  # POST: /session
+  # POST: /session.json
   def create
     # supplier = Supplier.find_by :name =>
     # params[:name]
-      supplier = Supplier.find_by :email =>
-      params[:email]
+    p "======================================================="
+    p params[:email]
+    p params[:password]
+    supplier = Supplier.find_by :email =>
+    params[:email]
     if supplier.present? &&   supplier.authenticate(params[:password])
 
       session[:supplier_id] = supplier.id
-      redirect_to root_path
-      else
+      render :action => 'index.json'
+      # redirect_to root_path
+    else
       flash[:error] = "Invalid email or password"
       redirect_to login_path
     end
--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,26 +1,15 @@
 class SuppliersController < ApplicationController
-  # before_action :check_for_admin, :only => [:index]
-  skip_before_action :verify_authenticity_token, :only => [:search]
-  # before_action :check_for_login, :only => [:show, :invite, :new, :create]
+ # before_action :check_for_admin, :only => [:index]
+ skip_before_action :verify_authenticity_token, :only => [:search]
+ # before_action :check_for_login, :only => [:show, :invite, :new, :create]
 
+ def search
 
-  # POST /suppliers
-  # POST /suppliers.json
-  def search
-    @suppliers = if params[:geocode].present?
-                   lat = params[:geocode][:latitude]
-                   lng = params[:geocode][:longitude]
-                   radius = params[:radius]? params[:radius] : 10
-                   Supplier.near([lat, lng], radius, units: :km)
-                 else
-                   Supplier.all
-                 end
-    if params[:skill_category].present?
-
-    end
-    render :action => 'search.json'
+  @params = params
+  respond_to do |format|
+    format.json { render :search}
   end
-
+ end
 
  def index
   @suppliers = Supplier.all
@@ -30,19 +19,19 @@ class SuppliersController < ApplicationController
   @supplier = Supplier.new
  end
 
-  # def create
-  #  @suppliers = Supplier.new user_params
-  #  if @supplier.save
-  #    session[:supplier_id] = @supplier.id
-  #    redirect_to new_home_path                    # ask boys about path
-  #  else
-  #    render :new
-  #  end
-  # end
+ # def create
+ #  @suppliers = Supplier.new user_params
+ #  if @supplier.save
+ #    session[:supplier_id] = @supplier.id
+ #    redirect_to new_home_path                    # ask boys about path
+ #  else
+ #    render :new
+ #  end
+ # end
 
   def create
    @supplier = Supplier.new(supplier_params)
-
+    
     respond_to do |format|
      if @supplier.save
       # SupplierMailer.welcome(@supplier).deliver_now
@@ -59,8 +48,7 @@ class SuppliersController < ApplicationController
       params.require(:supplier).permit(:name, :address, :latitude, :longitude, :email, :password, :password_confirmation)
     end
 
-  private
-  def supplier_params
-    params.require(:supplier).permit(:name, :email, :password, :password_confirmation)
-  end
+    def search_params
+      params.require(:flight).permit(:plane_id, :flight_number, :from, :to, :depart_dt)
+    end
 end
